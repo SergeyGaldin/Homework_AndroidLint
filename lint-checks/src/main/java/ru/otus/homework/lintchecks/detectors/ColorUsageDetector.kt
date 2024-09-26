fd924b1f42713ac752001c45e1b186da067441ae
GaldinSD <galdinsd@cdisoft.ru>
2024-09-26 11:21:42 +0300

    ColorUsageDetector

--- lint-checks/src/main/java/ru/otus/homework/lintchecks/detectors/ColorUsageDetector.kt
+++ lint-checks/src/main/java/ru/otus/homework/lintchecks/detectors/ColorUsageDetector.kt
@@ -1,18 +1,29 @@
 package ru.otus.homework.lintchecks.detectors
 
+import com.android.resources.ResourceFolderType
 import com.android.tools.lint.detector.api.Category
+import com.android.tools.lint.detector.api.Context
 import com.android.tools.lint.detector.api.Implementation
+import com.android.tools.lint.detector.api.Incident
 import com.android.tools.lint.detector.api.Issue
+import com.android.tools.lint.detector.api.LintFix
+import com.android.tools.lint.detector.api.Location
 import com.android.tools.lint.detector.api.ResourceXmlDetector
 import com.android.tools.lint.detector.api.Scope
 import com.android.tools.lint.detector.api.Severity
+import com.android.tools.lint.detector.api.XmlContext
+import com.android.tools.lint.detector.api.XmlScannerConstants
+import org.w3c.dom.Attr
+import org.w3c.dom.Element
 
 @Suppress("UnstableApiUsage")
-class ColorUsageDetector: ResourceXmlDetector() {
+class ColorUsageDetector : ResourceXmlDetector() {
+    private val arbitraryColors = ArrayList<Pair<String, Location>>()
+    private val colorMap = HashMap<String, ArrayList<String>>()
 
     companion object {
         val ISSUE: Issue = Issue.create(
-            id = "JobInBuilderUsage",
+            id = "ColorUsage",
             briefDescription = "Используемые цвета должны браться из палитры",
             explanation = """
             Все цвета, которые используются в ресурсах приложения должны находится в палитре. 
@@ -27,6 +38,65 @@ class ColorUsageDetector: ResourceXmlDetector() {
         )
     }
 
+    override fun getApplicableAttributes(): Collection<String> = XmlScannerConstants.ALL
 
+    override fun getApplicableElements(): Collection<String> = XmlScannerConstants.ALL
+
+    override fun appliesTo(folderType: ResourceFolderType) =
+        folderType == ResourceFolderType.LAYOUT ||
+                folderType == ResourceFolderType.DRAWABLE ||
+                folderType == ResourceFolderType.COLOR ||
+                folderType == ResourceFolderType.VALUES
+
+    override fun visitAttribute(context: XmlContext, attribute: Attr) {
+        val value = attribute.value ?: return
+        if (value.isColor()) {
+            arbitraryColors.add(value.lowercase() to context.getValueLocation(attribute))
+        }
+    }
+
+    override fun visitElement(context: XmlContext, element: Element) {
+        if (!context.file.path.contains("colors.xml")) return
+        val value = element.firstChild.nodeValue.lowercase()
+        if (value.isColor()) {
+            val colorName = element.attributes.item(0)?.nodeValue ?: return
+            if (!colorMap.containsKey(value)) {
+                colorMap[value] = ArrayList()
+            }
+            colorMap[value]!!.add(colorName)
+        }
+    }
+
+    override fun beforeCheckRootProject(context: Context) {
+        arbitraryColors.clear()
+        colorMap.clear()
+    }
+
+    override fun afterCheckRootProject(context: Context) {
+        arbitraryColors.forEach {
+            val fixValues = colorMap[it.first]
+
+            context.report(
+                ISSUE,
+                it.second,
+                "BRIEF_DESCRIPTION",
+                quickfixData = createFix(fixValues, it.second)
+            )
+        }
+    }
+
+    private fun createFix(
+        fixValues: List<String>?,
+        location: Location
+    ) = fixValues?.let {
+        fix()
+            .replace()
+            .range(location)
+            .all()
+            .with("@color/$it")
+            .build()
+    }
+
+    private fun String.isColor(): Boolean = (length == 7 || length == 9) && startsWith("#")
 
 }
